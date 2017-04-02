aba30b2ceb3b62bd34aef805135fb0d5c97d09ac
Ana Ausek <anac_ausek@hotmail.com>
2017-04-01 22:42:39 -0400

    first push

--- js/dixby.js
+++ js/dixby.js
@@ -1,19 +1,36 @@
-//Require Twitter Package
+//Global variables
+var command = process.argv[2];
 var TwitterPackage = require("twitter");
-var client = require("./keys.js");
+var keys = require("./keys.js");
+
+//Switch statement for all functions
 
 //Grab  Twitter keys from keys.js file
-var userKeys = client.twitterKeys
-console.log(userKeys);
+var client = new TwitterPackage(keys.twitterKeys);
+
+//Setting up parameters for Twitter request
+var params = {screen_name: "AusekAna", count: 20};
 
 //Callback to get Twitter Stream
-userKeys.stream("statuses/filter", {track: "javascript"}, function(stream){
-	stream.on("data", function(tweet){
-		console.log(event && event.text);
-	});
-	stream.on("error", function(error){
-		throw error;
-	});
+client.get("statuses/user_timeline", params, function(error, tweets, response) {
+  
+  //Conditional allowing user command to display 20 latest tweets
+  if (command === "my-tweets") {
+
+  	//Looping through the tweets array
+    for (var i = 0; i < tweets.length; i++) {
+    	console.log(JSON.stringify(tweets[i].text));
+    }
+  }else {
+  	throw error;
+  }
+  
+
+
 });
 
+//console.log(JSON.stringify(response));
 
+//Function for Spotify
+
+//Function for OMDB
